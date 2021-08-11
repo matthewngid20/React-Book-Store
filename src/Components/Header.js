ee9eeb7316dad890850d1479fc322512eeff8e16
Rica <ricamaeaverion22@gmail.com>
2021-08-11 17:44:54 +1000

    updated navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,8 +10,11 @@ export function Header(props) {
   })
   
   return (
+
     <div className="navbar navbar-expand-lg navbar-dark bg-dark">
+
       <div className="container-fluid">
+
         <Link className="navbar-brand order-0 order-sm-0 order-md-0" to="/">{props.name}</Link>
         <button 
           className="navbar-toggler order-3 order-sm-4" 
@@ -24,31 +27,39 @@ export function Header(props) {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <form className="d-flex flex-fill my-3 my-md-0 flex-fill order-4 order-sm-3 order-md-2">
           <input className="form-control me-md-2" type="search" placeholder="Search for books" aria-label="Search" />
           <button className="btn btn-outline-success" type="submit">
             <i className="fas fa-search"></i>
           </button>
         </form>
+
         <nav 
           className="navbar navbar-nav flex-row mx-2 order-2 order-sm-2 order-md-3" 
           style={{display:(props.auth) ? "flex" : "none"}}
         >
-          <Link className="nav-link px-3 py-sm-0 " to="/profile">
-            <span className="d-none d-sm-inline-block mx-2">Profile</span>
-            <i className="fas fa-user"></i>
-          </Link>
+          
           <Link className="nav-link px-3 py-sm-0" to="/favourites">
-            <span className="d-none d-sm-inline-block mx-2">Favourites</span>
+            <span className="d-none d-sm-inline-block mx-2">My List</span>
             <i className="fas fa-heart"></i>
           </Link>
+          <Link className="nav-link px-3 py-sm-0 " to="/profile">
+            <span className="d-none d-sm-inline-block mx-2"></span>
+            <i className="fas fa-user-circle"></i>
+          </Link>
+          <Link className="nav-link px-3 py-sm-0 " to="#">
+            <span className="d-none d-dm-inline-block mx-2"></span>
+            <i className="fas fa-shopping-cart"></i>
+          </Link>
         </nav>
-        <div className="collapse navbar-collapse order-5 order-sm-5 order-md-5" id="navbarSupportedContent">
-          <nav className="navbar-nav ms-auto mb-2 mb-lg-0">
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <nav className="navbar-nav">
             {SiteNav}
           </nav>
         </div>
       </div>
-    </div>
+    </div> 
   )
 }

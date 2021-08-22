e278fa82297c4ee6b677f0c0cc7921d4f5aff429
matthewngid20 <matthew.ng.id20@gmail.com>
2021-08-22 14:32:31 +1000

    new changes for user update

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useState } from 'react/cjs/react.development'
+import "firebase/auth"
+import firebase from 'firebase'
+import { IsConstructor } from 'es-abstract'
 
-export const Profile = () => {
+
+export const Profile = (props) => {
+
+    const [password, setPassword] = useState()
+
+    const user = firebase.auth().currentUser
+    
+
+
+  
+
+
     return (
         <>
-        <form>
+        <form  >
             <div className="mb-3">
                 <label for="email" class="form-label">Email address</label>
                 <input type="email" class="form-control" id="email" aria-describedby="emailHelp"/>
@@ -11,17 +26,17 @@ export const Profile = () => {
             </div>
             <div className="mb-3">
                 <label for="password" class="form-label">Old Password</label>
-                <input type="password" class="form-control" id="password"/>
+                <input type="password" class="form-control" id="password" name="oldpassword" />
             </div>
             <div className="mb-3">
                 <label for="password" class="form-label">New Password</label>
-                <input type="password" class="form-control" id="password"/>
+                <input type="password" class="form-control" name="newpassword" id="password"/>
             </div>
             <div className="mb-3">
                 <label for="confirmedPassword" class="form-label">Confirm Password</label>
-                <input type="password" class="form-control" id="confirmedPassword"/>
+                <input type="password" class="form-control" name="newpasswordconfirm" id="confirmedPassword"/>
             </div>
-            <button type="submit" class="btn btn-primary">Update</button>
+            <button type="submit" class="btn btn-primary" >Update</button>
         </form>
         </>
     )
